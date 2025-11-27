b98ab0fb245e072fce380e833b555dbc9638b91d
Peter Backeman <peter@backeman.se>
2025-11-27 19:48:14 +0100

    Add dilemma example

---
+++ examples/dilemma.c
@@ -0,0 +1,20 @@
+void main() {
+    int a;
+    int b;
+    int r;
+    a = 1;
+    
+    if (a == 1) {
+        b = 1;
+    } else {
+        b = 0;
+    }
+
+    if (b == 1) {
+        r = 1;
+    } else {
+        r = 1;
+    }
+
+    assert(r == 1);
+}
